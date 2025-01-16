aa762f90acb6a3993f8c05692c5712737ba66b4d
omasebabu <115346280+omasebabu@users.noreply.github.com>
2025-01-16 22:54:10 +0530

    Add files via upload

---
+++ AssignmentOperator.java
@@ -0,0 +1,16 @@
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		int number=10;//assignment operator (=):will assign 10 values to number variable
+		System.out.println("Number is:"+number);
+		number+=20;
+		System.out.println("After Adding 20 Number is:"+number);
+		number-=10;
+		System.out.println("After minus 20 number is:"+number);
+		number*=5;
+		System.out.println("After multiply 5 number is:"+number);
+		number/=12;
+		System.out.println("After divide 12 number is:"+number);
+	}
+
+}
